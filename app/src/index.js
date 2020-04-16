5bf46e61176182bb7d4cc7fcfe27fd26fcbc5f28
Desi Curry <56737796+desicurry@users.noreply.github.com>
2020-04-15 22:21:39 -0400

    MVP Complete

--- app/src/index.js
+++ app/src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { dogPicReducer } from './reducers/dogPicReducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(dogPicReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+<Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
